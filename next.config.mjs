212a9e8b6815f24105d44eb8b9b406294187ca2a
Ui-Dev-Ram <145400237+Ui-Dev-Ram@users.noreply.github.com>
2024-09-04 10:30:54 +0530

    add some deploy codes

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@
 const config = {
     images: {
       unoptimized: true,
-      domains: ['www.denofgeek.com', 'images.unsplash.com', "api.microlink.io", "127.0.0.1" /* Add other domains here if needed */],
+      domains: ['www.denofgeek.com', 'images.unsplash.com', "api.microlink.io", "127.0.0.1", "radiant-luck-2d3eaaac89.media.strapiapp.com" /* Add other domains here if needed */],
     },
   };
 
